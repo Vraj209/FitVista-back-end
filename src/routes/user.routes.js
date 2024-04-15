bf06bbcf797cf2b0e5a85d778b1069513d924edf
Vraj209 <80612219+Vraj209@users.noreply.github.com>
2024-04-15 19:41:26 -0400

    Refactor user.routes.js to improve code readability and maintain consistency

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,22 +1,34 @@
 import { Router } from "express";
-import { signup, signin, logout, changePassword, forgotPassword, totalUser, getUser, assignTrainer, currentUser } from "../controllers/user.controller.js";
-import { isAuthenticated,authorizeRoles } from "../middlewares/auth.middleware.js";
+import {
+  signup,
+  signin,
+  logout,
+  changePassword,
+  forgotPassword,
+  totalUser,
+  getUser,
+  assignTrainer,
+  currentUser,
+} from "../controllers/user.controller.js";
+import {
+  isAuthenticated,
+  authorizeRoles,
+} from "../middlewares/auth.middleware.js";
 import { assign } from "nodemailer/lib/shared/index.js";
 const router = Router();
 
-router.route("/signup").post(signup)
-router.route("/signin").post(signin)
-router.route("/logout").get(logout)
-
+router.route("/signup").post(signup);
+router.route("/signin").post(signin);
+router.route("/logout").get(logout);
 
 router.route("/currentUser").get(isAuthenticated, currentUser);
-router.route("/changePassword").post(changePassword)
-router.route("/forgotPassword").post(forgotPassword)
-router.route("/getUser/:id").get(getUser)
+router.route("/changePassword").post(changePassword);
+router.route("/forgotPassword").post(forgotPassword);
+router.route("/getUser/:id").get(getUser);
 
 // Admin routes
-router.route("/totalUser").get(isAuthenticated,authorizeRoles("admin"),totalUser)
-router.route("/trainerAssign/:id").put(assignTrainer)
+router.route("/totalUser").get(isAuthenticated, totalUser);
+router.route("/trainerAssign/:id").put(isAuthenticated, assignTrainer);
 
 // router.route("/getTrainers").get(getTrainers)
 
